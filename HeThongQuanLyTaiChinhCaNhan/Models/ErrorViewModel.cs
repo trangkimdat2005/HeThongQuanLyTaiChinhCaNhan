c77c7bb4e1ebe835e45d71ab40c44abf00f1bcb0
ngominhkhoi05 <khoi22092021@gmail.com>
2026-01-11 03:01:25 +0700

    chuyển sang web cho đẹp với hi vọng cô sẽ cho điểm cao hehe

---
+++ HeThongQuanLyTaiChinhCaNhan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HeThongQuanLyTaiChinhCaNhan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
