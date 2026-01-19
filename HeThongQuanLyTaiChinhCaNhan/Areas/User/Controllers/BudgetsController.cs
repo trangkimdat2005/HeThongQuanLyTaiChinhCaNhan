aea024ab195d7124d008f242a858cff6e423f7dd
trangkimdat2005 <trangkimdatst2005@gmail.com>
2026-01-19 22:16:13 +0700

    dẫn luồng cho user

---
+++ HeThongQuanLyTaiChinhCaNhan/Areas/User/Controllers/BudgetsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeThongQuanLyTaiChinhCaNhan.Areas.User.Controllers
+{
+    [Area("User")]
+    [Route("User/Budgets")]
+    public class Budgetscontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Budgets");
+        }
+    }
+}
---
+++ HeThongQuanLyTaiChinhCaNhan/Areas/User/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeThongQuanLyTaiChinhCaNhan.Areas.User.Controllers
+{
+    [Area("User")]
+    [Route("User/Categories")]
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Categories");
+        }
+    }
+}
