d9c227ef6863419ffb011ef542f1cb2e52a2df97
ngominhkhoi05 <khoi22092021@gmail.com>
2026-01-20 01:17:32 +0700

    fix thay ba luon

--- HeThongQuanLyTaiChinhCaNhan/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HeThongQuanLyTaiChinhCaNhan.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- HeThongQuanLyTaiChinhCaNhan/Services/Interfaces/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HeThongQuanLyTaiChinhCaNhan.Services.Interfaces
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string content);
-    }
-}
